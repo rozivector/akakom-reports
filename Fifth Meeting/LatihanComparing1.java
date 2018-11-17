f84be05f8b60b5283a35723b67a20a8c923c7e44
Rozi Vector <rozi.qood16@gmail.com>
2018-11-17 16:10:32 +0700

    Create LatihanComparing1.java

---
+++ Fifth Meeting/LatihanComparing1.java
@@ -0,0 +1,11 @@
+public class LatihanComparing1
+{
+	public static void main(String[] args)
+	{
+	int x = 3;
+	int y = 2;
+	int z = x+y;
+	boolean test = (z==x+y);
+	System.out.println(test);
+	}
+}

182736a18ff56c1e3377c00d50b0b3e792f5a0e0
Rozi Vector <rozi.qood16@gmail.com>
2018-11-17 15:53:11 +0700

    Create Text01.java

---
+++ First Meeting/Text01.java
@@ -0,0 +1,11 @@
+public class Text01
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My name is Rozi");
+		System.out.println();
+		System.out.println("From TI class 1");
+		System.out.println("Nice to meet you!");
+
+	}
+}

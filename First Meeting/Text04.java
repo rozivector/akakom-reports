d9a2c067e43224b6635412436b5bd2f613d055b1
Rozi Vector <rozi.qood16@gmail.com>
2018-11-17 15:57:38 +0700

    Add files via upload

---
+++ First Meeting/Text04.java
@@ -0,0 +1,15 @@
+public class Text04
+{
+	public static void main(String[] args)
+	{
+	String x = "Cat";
+	System.out.println(x);
+	x = "Bunny";
+	System.out.println(x);
+	System.out.println();
+	String y = "Cat";
+	System.out.println(x);
+	System.out.println(y);
+
+	}
+}

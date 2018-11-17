c83166753e6a073e723e5b6b9d5dbd9aff93c695
Rozi Vector <rozi.qood16@gmail.com>
2018-11-17 15:55:53 +0700

    Add files via upload

---
+++ First Meeting/Text02.java
@@ -0,0 +1,12 @@
+public class Text02
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My name is Rozi");
+		System.out.println("Nice to meet you!");
+		System.out.println("Be nice and respect!" + "\n"
+		+ "Today i am still noob" + "\n"
+		+ "Do not ask me, ok?" + "\n"
+		+ "Rozi is the best!");
+	}
+}

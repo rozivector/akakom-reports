6d3bdc90c4bdfa3c8753134870c82e91359ad9dc
Rozi Vector <rozi.qood16@gmail.com>
2018-11-17 16:03:02 +0700

    Create TipeData1.java

---
+++ Second Meeting/TipeData1.java
@@ -0,0 +1,16 @@
+public class TipeData1
+{
+	public static void main(String[] args)
+	{
+	int x=20;
+	System.out.println("x= "+x);
+	x=25;
+	System.out.println("x= "+x);
+	x=x+1;
+	System.out.println("x= "+x);
+	x+=1;
+	System.out.println("x= "+x);
+	x++;
+	System.out.println("x= "+x);
+	};
+};

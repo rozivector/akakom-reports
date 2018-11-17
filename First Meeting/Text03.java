aab284763aec76d5f1e80c7b0e223ba24c4ed303
Rozi Vector <rozi.qood16@gmail.com>
2018-11-17 15:56:49 +0700

    Add files via upload

---
+++ First Meeting/Text03.java
@@ -0,0 +1,13 @@
+public class Text03
+{
+	public static void main(String[] args)
+	{
+	String x = "Rozi";
+	System.out.println("My name is " + x);
+	System.out.println(x + ", So cool");
+	System.out.println("Go " + x + "\n \n"
+	+ "It's " + x + " party! \n"
+	+ "Hail " + x + "\n"
+	+ "Yo, " + x + " Is the best!");
+	}
+}
